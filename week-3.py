95b1b963eba3928175913069ffcebe368d6b9788
ilker yilmaz <ilkery549@gmail.com>
2022-10-17 13:57:04 +0300

    week-4

--- week-3.py
+++ week-3.py
@@ -1,83 +1,83 @@
-print("######################### SORU - 1 #########################")
-# Kullanıcıdan integer türünde bir değer isteyiniz. İstemiş olduğunuz bu değerin çarpım tablosu
-# değerlerini gösteren kodu for döngüsü ile gerçekleştiriniz
-# Örnek: Kullanıcı 5 değerini girerse, çıktı olarak 5 10 15 20 … … 50 (10’a kadar)
-
-# Kullanıcıdan integer türünde bir değer isteme
-sayi = int(input("Sayı: "))
-# İstemiş olduğunuz bu değerin çarpım tablosu değerlerini gösteren kodu for döngüsü ile gerçekleştirme
-for i in range(1,11):
-    print("{} x {} = {}".format(sayi,i,sayi*i))
-
-print("######################### SORU - 2 #########################")
-# Girilen bir sayının kaç basamaklı olduğunu belirleyen programı while döngüsü ile gerçekleştiriniz
-
-# Girilen bir sayının kaç basamaklı olduğunu belirleme
-sayi = int(input("Sayı: "))
-basamak = 0
-while sayi > 0:
-    sayi = sayi // 10
-    basamak += 1
-print("Basamak sayısı: {}".format(basamak))
-
-print("######################### SORU - 3 #########################")
-# Aşağıda bir listeye ait sayısal değerler verilmiştir.
+# print("######################### SORU - 1 #########################")
+# # Kullanıcıdan integer türünde bir değer isteyiniz. İstemiş olduğunuz bu değerin çarpım tablosu
+# # değerlerini gösteren kodu for döngüsü ile gerçekleştiriniz
+# # Örnek: Kullanıcı 5 değerini girerse, çıktı olarak 5 10 15 20 … … 50 (10’a kadar)
+#
+# # Kullanıcıdan integer türünde bir değer isteme
+# sayi = int(input("Sayı: "))
+# # İstemiş olduğunuz bu değerin çarpım tablosu değerlerini gösteren kodu for döngüsü ile gerçekleştirme
+# for i in range(1,11):
+#     print("{} x {} = {}".format(sayi,i,sayi*i))
+#
+# print("######################### SORU - 2 #########################")
+# # Girilen bir sayının kaç basamaklı olduğunu belirleyen programı while döngüsü ile gerçekleştiriniz
+#
+# # Girilen bir sayının kaç basamaklı olduğunu belirleme
+# sayi = int(input("Sayı: "))
+# basamak = 0
+# while sayi > 0:
+#     sayi = sayi // 10
+#     basamak += 1
+# print("Basamak sayısı: {}".format(basamak))
+#
+# print("######################### SORU - 3 #########################")
+# # Aşağıda bir listeye ait sayısal değerler verilmiştir.
+# # sayısalDeğerler = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
+# # Bu listedeki 5’e bölünen sayıları çıktı olarak veren programı hem for hem de while döngüsü ile
+# # gerçekleştiriniz. 150’den büyük değerleri dikkate almayınız (20p).
+# # Çıktı: 15, 55, 75, 150
+#
+# # Aşağıda bir listeye ait sayısal değerler verilmiştir.
 # sayısalDeğerler = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
-# Bu listedeki 5’e bölünen sayıları çıktı olarak veren programı hem for hem de while döngüsü ile
-# gerçekleştiriniz. 150’den büyük değerleri dikkate almayınız (20p).
-# Çıktı: 15, 55, 75, 150
-
-# Aşağıda bir listeye ait sayısal değerler verilmiştir.
-sayısalDeğerler = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
-
-# for döngüsü ile gerçekleştirme
-for i in sayısalDeğerler:
-    if i > 150:
-        break
-    if i % 5 == 0:
-        print(i, end=", ")
-print()
-# while döngüsü ile gerçekleştirme
-i = 0
-while i < len(sayısalDeğerler):
-    if sayısalDeğerler[i] > 150:
-        break
-    if sayısalDeğerler[i] % 5 == 0:
-        print(sayısalDeğerler[i], end=", ")
-    i += 1
-print()
-
-print("######################### SORU - 4 #########################")
-# Kullanıcıdan 3 adet (a, b ve c) değer alınız. a (dahil) ve b (dahil) arasında kaç sayının c’ye
-# bölünebildiğini belirleyen programı yazınız (20p).
-# Örnek: a = 20, b = 40, c = 5 ise Çıktı: 5
-
-# Kullanıcıdan 3 adet (a, b ve c) değer alma
-a = int(input("a: "))
-b = int(input("b: "))
-c = int(input("c: "))
-
-# a (dahil) ve b (dahil) arasında kaç sayının c’ye bölünebildiğini belirleme
-sayac = 0
-for i in range(a,b+1):
-    if i % c == 0:
-        sayac += 1
-print("Bölünebilen sayı sayısı: {}".format(sayac))
-
-print("######################### SORU - 5 #########################")
-# Aşağıdaki çıktıyı veren programı yazınız (10p).
-# 1 – 99
-# 2 – 98
-# 3 – 97
-# ..
-# ..
-# ..
-# 98 – 2
-# 99 – 1
-
-# Aşağıdaki çıktıyı veren programı yazma
-for i in range(1,100):
-    print("{} - {}".format(i,100-i))
+#
+# # for döngüsü ile gerçekleştirme
+# for i in sayısalDeğerler:
+#     if i > 150:
+#         break
+#     if i % 5 == 0:
+#         print(i, end=", ")
+# print()
+# # while döngüsü ile gerçekleştirme
+# i = 0
+# while i < len(sayısalDeğerler):
+#     if sayısalDeğerler[i] > 150:
+#         break
+#     if sayısalDeğerler[i] % 5 == 0:
+#         print(sayısalDeğerler[i], end=", ")
+#     i += 1
+# print()
+#
+# print("######################### SORU - 4 #########################")
+# # Kullanıcıdan 3 adet (a, b ve c) değer alınız. a (dahil) ve b (dahil) arasında kaç sayının c’ye
+# # bölünebildiğini belirleyen programı yazınız (20p).
+# # Örnek: a = 20, b = 40, c = 5 ise Çıktı: 5
+#
+# # Kullanıcıdan 3 adet (a, b ve c) değer alma
+# a = int(input("a: "))
+# b = int(input("b: "))
+# c = int(input("c: "))
+#
+# # a (dahil) ve b (dahil) arasında kaç sayının c’ye bölünebildiğini belirleme
+# sayac = 0
+# for i in range(a,b+1):
+#     if i % c == 0:
+#         sayac += 1
+# print("Bölünebilen sayı sayısı: {}".format(sayac))
+#
+# print("######################### SORU - 5 #########################")
+# # Aşağıdaki çıktıyı veren programı yazınız (10p).
+# # 1 – 99
+# # 2 – 98
+# # 3 – 97
+# # ..
+# # ..
+# # ..
+# # 98 – 2
+# # 99 – 1
+#
+# # Aşağıdaki çıktıyı veren programı yazma
+# for i in range(1,100):
+#     print("{} - {}".format(i,100-i))
 
 print("######################### SORU - 6 #########################")
 # Kullanıcıdan bir IP adresi isteyiniz. İstediğiniz bu IP adresinden sonraki 5 değeri çıktı olarak
--- week-4.py
+++ week-4.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
 # Veri setinin yüklenmesi
-veriler = pd.read_csv("datasets/telefon_fiyatlari.csv")
+veriler = pd.read_csv("datasets/mobile-price/train.csv")
 
 # sınıf sayısının belirlenmesi
 label_encoder = LabelEncoder().fit(veriler.price_range)
@@ -75,3 +75,97 @@ plt.show()
 # 4) Diyabet verisetini kullanarak bir YSA modeli oluşturunuz. Bu YSA modeline, eğitim ve doğrulama
 # işlemlerinin başarım ve kayıplarını belirleyiniz. Bu değerleri bir grafik üzerinde gösteriniz (20p).
 # 5) Soru 4’te oluşturduğunuz modelin ROC eğrisini çiziniz (30p).
+
+# Verilen kodu inceleyerek kendi modelinizi oluşturunuz
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+model = Sequential() # modelin oluşturulması
+model.add(Dense(16, input_dim=20, activation="relu"))
+model.add(Dense(12, activation="relu")) # gizli katman
+model.add(Dense(4, activation="softmax")) # çıktı katmanı
+model.summary() # modelin özetini gösterir
+
+# Verilen kodu inceleyerek çapraz doğrulama işlemi yapınız ve buna göre başarımı değerlendiriniz
+
+from sklearn.model_selection import cross_val_score
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+
+def build_classifier():
+    model = Sequential() # modelin oluşturulması
+    model.add(Dense(16, input_dim=20, activation="relu"))
+    model.add(Dense(12, activation="relu")) # gizli katman
+    model.add(Dense(4, activation="softmax")) # çıktı katmanı
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"]) # modelin derlenmesi
+    return model # modelin döndürülmesi
+
+classifier = KerasClassifier(build_fn=build_classifier, epochs=150) # modelin oluşturulması
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10) # çapraz doğrulama işlemi
+print("Average Accuracy: ", accuracies.mean()) # ortalama başarımların gösterilmesi
+print("Standard Deviation: ", accuracies.std()) # standart sapmaların gösterilmesi
+
+# Verisetinde (telefon_fiyat_değişimi) toplam 20 adet özellik bulunmaktadır. Bu verisetinden
+# “blue”, “fc”, “int_memory”, “ram” ve “wifi” değerlerini çıkarıp, sınıflandırma işlemini tekrar
+# yapınız
+
+veriler = pd.read_csv("telefon_fiyat_değişimi.csv") # verilerin okunması
+veriler = veriler.drop(["blue", "fc", "int_memory", "ram", "wifi"], axis=1) # verilerden özelliklerin çıkarılması
+X = veriler.drop(["price_range"], axis=1) # bağımsız değişkenlerin oluşturulması
+y = veriler["price_range"] # bağımlı değişkenin oluşturulması
+
+# Diyabet verisetini kullanarak bir YSA modeli oluşturunuz. Bu YSA modeline, eğitim ve doğrulama
+# işlemlerinin başarım ve kayıplarını belirleyiniz. Bu değerleri bir grafik üzerinde gösteriniz
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
+veriler = pd.read_csv("datasets/diabetes.csv") # verilerin okunması
+X = veriler.drop(["Outcome"], axis=1) # bağımsız değişkenlerin oluşturulması
+y = veriler["Outcome"] # bağımlı değişkenin oluşturulması
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42) # verilerin eğitim ve test olarak bölünmesi
+
+sc = StandardScaler() # verilerin ölçeklendirilmesi
+X_train = sc.fit_transform(X_train)  # eğitim verilerinin ölçeklendirilmesi
+X_test = sc.transform(X_test) # verilerin ölçeklendirilmesi
+
+model = Sequential() # modelin oluşturulması
+model.add(Dense(12, input_dim=8, activation="relu")) # giriş katmanı
+model.add(Dense(8, activation="relu")) # gizli katman
+model.add(Dense(1, activation="sigmoid")) # çıktı katmanı
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]) # modelin derlenmesi
+model.fit(X_train, y_train, epochs=150, batch_size=10, validation_data=(X_test, y_test)) # modelin eğitilmesi
+
+plt.plot(model.history.history["accuracy"]) # eğitim başarımının gösterilmesi
+plt.plot(model.history.history["val_accuracy"]) # doğrulama başarımının gösterilmesi
+plt.title("Model Başarımları") # başlık
+plt.ylabel("Başarımlar") # y ekseninin adı
+plt.xlabel("Epok Sayısı") # x ekseninin adı
+plt.legend(["Eğitim", "Test"], loc="upper left") # gösterilecek değerlerin belirlenmesi
+plt.show() # grafik gösterimi
+
+plt.plot(model.history.history["loss"]) # eğitim kaybının gösterilmesi
+plt.plot(model.history.history["val_loss"]) # doğrulama kaybının gösterilmesi
+plt.title("Model Kayıpları") # başlık
+plt.ylabel("Kayıplar") # y ekseninin adı
+plt.xlabel("Epok Sayısı") # x ekseninin adı
+plt.legend(["Eğitim", "Test"], loc="upper left") # gösterilecek değerlerin belirlenmesi
+plt.show() # grafik gösterimi
+
+# Soru 4’te oluşturduğunuz modelin ROC eğrisini çiziniz
+
+from sklearn.metrics import roc_curve
+y_pred = model.predict(X_test) # modelin tahmin etmesi
+fpr, tpr, thresholds = roc_curve(y_test, y_pred) # ROC eğrisinin oluşturulması
+plt.plot(fpr, tpr) # ROC eğrisinin gösterilmesi
+plt.plot([0, 1], [0, 1], linestyle="--") # eğriyi çizdirmek için
+plt.xlabel("False Positive Rate") # x ekseninin adı
+plt.ylabel("True Positive Rate") # y ekseninin adı
+plt.title("ROC Curve") # başlık
+plt.show() # grafik gösterimi
+
+
